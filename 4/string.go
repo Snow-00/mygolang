b4dd1081f2d3b459460fd95efd0492789ba5a33c
Snow-00 <wariorwilly43@gmail.com>
2023-02-10 00:20:14 +0000

    string - 4

---
+++ 4/string.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+  fmt.Println("testing, testing")
+  fmt.Println(len("testing"))
+  fmt.Println("testing"[0])
+  fmt.Println("testing coba liat"[1])
+  fmt.Println("testing, coba, liat"[1])
+}
