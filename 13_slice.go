519dddb4e9c2c967eedaabb5e8b5b8ce1c661ada
Snow-00 <wariorwilly43@gmail.com>
2023-02-14 16:28:31 +0000

    slice

---
+++ 13_slice.go
@@ -0,0 +1,38 @@
+// slice bagian dari array ex: array[1:10]
+// slice terhubung dengan array sehingga jika data di array brubah
+// maka isi slice juga brubah
+// slice terdiri dari 3 bagian = pointer, length, capacity
+
+package main
+import "fmt"
+
+func main(){
+  // jika tidak tau jumlah data di array bisa menggunakan ... dahulu
+  var months = [...]string{
+    "Januari",
+    "Februari",
+    "Maret",
+    "april",
+    "mei",
+    "juni",
+    "juli",
+    "agustus",
+    "september",
+    "oktober",
+    "november",
+    "desember",
+  }
+
+  var slice1 = months[4:7]
+  fmt.Println(slice1)
+  fmt.Println(len(slice1))
+  fmt.Println(cap(slice1))
+
+  months[5] = "test"
+  fmt.Println(slice1)
+  
+  slice1[0] = "ubah"
+  fmt.Println(months)
+
+  days := [...]string{"senin", "selasa", "rabu", "kamis"}
+}
