ea9ee140a0d20367e1bc6167e86971be9ce0e25e
Snow-00 <wariorwilly43@gmail.com>
2023-02-20 00:26:21 +0000

    slice

--- 13_slice.go
+++ 13_slice.go
@@ -22,7 +22,10 @@ func main(){
     "november",
     "desember",
   }
-
+  
+  // dlm membuat slice, jika menambahkan data > capacity, mk akan terbentuk
+  // array baru sehingga perubahan data di array br tdk akan mempengaruhi
+  // array asli
   var slice1 = months[4:7]
   fmt.Println(slice1)
   fmt.Println(len(slice1))
@@ -33,12 +36,26 @@ func main(){
   
   slice1[0] = "ubah"
   fmt.Println(months)
-
-  slice2 := months[10:]
+  
+  // capacity = dihitung dr data awal slice ke data akhir array
+  //slice2 := months[10:]
+  slice2 := months[2:4]
   fmt.Println(slice2)
+  //fmt.Println(cap(slice2))
 
   slice3 := append(slice2, "tambah")
   fmt.Println(slice3)
-  slice3[1] = "bukan desember" // 14:29
 
+  slice3[1] = "bukan desember"
+  fmt.Println(slice3)
+  fmt.Println(slice2)
+  fmt.Println(months)
+
+  newSlice := make([]string, 2, 5)
+
+  newSlice[0] = "test"
+  newSlice[1] = "coba"
+  fmt.Println(newSlice)
+  fmt.Println(len(newSlice))
+  fmt.Println(cap(newSlice))
 }
