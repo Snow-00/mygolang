0734360969168e799ed8d08d3d502dc4859a7502
Snow-00 <wariorwilly43@gmail.com>
2023-02-25 10:59:54 +0000

    switch finish

--- 16_switch.go
+++ 16_switch.go
@@ -15,4 +15,24 @@ func main(){
       fmt.Println("Kenalan yok")
       fmt.Println("Helo test")
   }
+
+  // short statement (local variable) di switch
+  //switch length := len(name); length > 5{
+  //  case true:
+  //    fmt.Println("nama terlalu panjang")
+  //  case false:
+  //    fmt.Println("nama benar")
+  //}
+
+  // switch tanpa kondisi, jd seperti kek if expression
+  // kondisi langsung msk ke case
+  length := len(name)
+  switch {
+    case length > 10:
+      fmt.Println("nama kepanjangan")
+    case length > 5:
+      fmt.Println("nama agak kepanjangan")
+    default:
+      fmt.Println("nama benar")
+  }
 }
